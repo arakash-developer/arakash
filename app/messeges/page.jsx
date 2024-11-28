eae1e1974b72ce43dfcdb929099515966712399f
akash100a <arakash968786@gmail.com>
2024-11-29 02:46:58 +0600

    ip

--- app/messeges/page.jsx
+++ app/messeges/page.jsx
@@ -1,47 +1,57 @@
 import React from 'react'
 import Container from '../component/layers/Container'
+import Link from 'next/link';
 
-const page = () => {
+const page = async () => {
+    let blob = await fetch("https://akashapi.vercel.app/read");
+    let response = await blob.json();
+    let messege = response.messege;
+    messege.reverse()
+    console.log(messege);
+
     return (
         <Container className='mt-10'>
-            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                    <caption class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <caption className="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
                         Client Messeges...
-                        {/* <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Browse a list of Flowbite products designed to help you work and play, stay organized, get answers, keep in touch, grow your business, and more.</p> */}
+                        {/* <p className="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Browse a list of Flowbite products designed to help you work and play, stay organized, get answers, keep in touch, grow your business, and more.</p> */}
                     </caption>
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 name
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Email
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Messeges
                             </th>
-                            <th scope="col" class="px-6 py-3">
-                                <span class="sr-only">Edit</span>
+                            <th scope="col" className="px-6 py-3">
+                                <span className="sr-only">Edit</span>
                             </th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Apple MacBook Pro 17"
-                            </th>
-                            <td class="px-6 py-4">
-                                Silver
-                            </td>
-                            <td class="px-6 py-4">
-                                Laptop lorem1000
-                            </td>
-                            <td class="px-6 py-4 text-right">
-                                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                            </td>
-                        </tr>
-
+                        {
+                            messege.map((meg) => (
+                                <tr key={meg._id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {meg.name}
+                                    </th>
+                                    <td className="px-6 py-4">
+                                        {meg.email}
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        {meg.messege}
+                                    </td>
+                                    <td className="px-6 py-4 text-right">
+                                        <Link href={`messeges/${meg._id}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</Link>
+                                    </td>
+                                </tr>
+                            ))
+                        }
                     </tbody>
                 </table>
             </div>
@@ -50,3 +60,4 @@ const page = () => {
 }
 
 export default page
+
