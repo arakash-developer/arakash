e8b5dcf1dadfc8e9f2f2ac7ee731dd05e463b480
akash100a <arakash968786@gmail.com>
2024-11-29 17:54:23 +0600

    up

---
+++ app/lib/getAllData.js
@@ -0,0 +1,19 @@
+const getAllData = async () => {
+    const blops = await fetch("https://akashapi.vercel.app/read",
+        {
+            // cache:"force-cache",
+            cache:"no-store",
+            // next: {
+            //     revalidate : 1,
+            // }    
+        }
+    )
+    if (!blops.ok) {
+        throw new Error("There Was An Error Fething Data")
+    } else {
+        return blops.json()
+    }
+
+    // returning promise
+}
+export default getAllData
