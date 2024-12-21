451fe5c07de534720461c82036e7edadca52254e
akash100a <arakash968786@gmail.com>
2024-12-21 16:27:03 +0600

    update

--- app/layout.js
+++ app/layout.js
@@ -287,6 +287,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className=''>
       <head>
+        <meta name="google-site-verification" content="j13FRaiJX0ckKou6S6sA4r2TMw4qsqA8oq3sjwgYnEc" />
         <link rel="icon" type="image/x-icon" href="./favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
