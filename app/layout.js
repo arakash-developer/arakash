5b46b2ee580ef03cd0b35d2c3db9723292e70687
akash100a <arakash968786@gmail.com>
2024-12-21 17:14:04 +0600

    uopop

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import DarkContext from "./contex/DarkContext";
 import "./globals.css";
 import "./app.css";
 import Akash from "@/public/akash5.png";
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 export const metadata = {
   title: "ATIQUR RAHMAN AKASH",
@@ -292,8 +294,11 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XNTETKSX4Y"></script>
       </head>
       <body>
+        <GoogleAnalytics gaId="G-XNTETKSX4Y" />
         <DarkContext>
           {children}
         </DarkContext>
