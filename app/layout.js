11ae58ea8370900d9e6b8d956947b1c977260f7a
akash100a <arakash968786@gmail.com>
2024-12-21 17:00:55 +0600

    aa

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import DarkContext from "./contex/DarkContext";
 import "./globals.css";
 import "./app.css";
 import Akash from "@/public/akash5.png";
-import { Html, Head, Main, NextScript } from 'next/document';
 
 export const metadata = {
   title: "ATIQUR RAHMAN AKASH",
@@ -283,32 +282,17 @@ I am a dedicated Next.js Developer with over 2.5 years of experience in designin
 
 
 export default function RootLayout({ children }) {
-  console.log(Akash);
 
   return (
     <html lang="en" className=''>
-      <Head>
+      <head>
         <meta name="google-site-verification" content="j13FRaiJX0ckKou6S6sA4r2TMw4qsqA8oq3sjwgYnEc" />
-        {/* Google Tag Manager */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-XNTETKSX4Y"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-XNTETKSX4Y');
-            `,
-          }}
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" type="image/x-icon" href="./favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
-      </Head>
+      </head>
       <body>
         <DarkContext>
           {children}
