3bdaf75e377ca1d7c9498fbf034e30df25227f6e
akash100a <arakash968786@gmail.com>
2024-12-21 16:42:07 +0600

    dghbzedsb n

--- app/layout.js
+++ app/layout.js
@@ -292,7 +292,7 @@ export default function RootLayout({ children }) {
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-XNTETKSX4Y"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){dataLayer.push(arguments)}
           gtag('js', new Date());
           gtag('config', 'G-XNTETKSX4Y');
         </script>
