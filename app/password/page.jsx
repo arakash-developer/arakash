d0a6c74d5d9dd608b540b51a902f827783d739a0
akash100a <arakash968786@gmail.com>
2024-12-24 02:26:34 +0600

    oo

--- app/password/page.jsx
+++ app/password/page.jsx
@@ -4,26 +4,7 @@ import Container from '../component/layers/Container'
 const page = () => {
   return (
     <Container>
-        <div className='my-5'>
-            <h1>Gmail </h1>
-            <p>gmail : arakash.figma@gmail.com</p>
-            <p>password : arakash556600aa5</p>
-        </div>
-        <div className='my-5'>
-            <h1>Figma </h1>
-            <p>gmail : arakash.figma@gmail.com</p>
-            <p>password : akashfigma556600</p>
-        </div>
-        <div className='my-5'>
-            <h1>Github </h1>
-            <p>gmail : arakash.figma@gmail.com</p>
-            <p>password : 9v2ketxb9hdj3vs</p>
-        </div>
-        <div className='my-5'>
-            <h1>NPM JS </h1>
-            <p>gmail : arakash.developer@gmail.com</p>
-            <p>password : akashnpmjsdeveloper556600</p>
-        </div>
+       
 
     </Container>
   )
