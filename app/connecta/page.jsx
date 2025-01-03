9202dd2d6227e12fc33c58a85bfbbab2b2cfde3a
akash100a <arakash968786@gmail.com>
2025-01-03 23:00:40 +0600

    d

--- app/connecta/page.jsx
+++ app/connecta/page.jsx
@@ -557,15 +557,15 @@ const page = () => {
               <div className="flex items-center justify-between px-4 py-2">
                 <div className="flex space-x-2 items-center">
                   <div className="relative">
-                    <img
-                      src="./images/avt-2.jpg"
+                    <Image
+                      src={Akash4}
                       alt="Profile picture"
                       className="w-10 h-10 rounded-full"
                     />
                     <span className="bg-green-500 w-3 h-3 rounded-full absolute right-0 top-3/4 border-white border-2" />
                   </div>
                   <div>
-                    <div className="font-semibold">John Doe</div>
+                    <div className="font-semibold">Atiqur Rahman Akash</div>
                     <span className="text-sm text-gray-500">38m</span>
                   </div>
                 </div>
