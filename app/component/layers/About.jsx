938f6f1c4e7faa3742cc6c58ac9b8c0c0f27ba5c
akashblinto <arakash968786@gmail.com>
2025-07-03 09:43:41 +0600

    fix: update title to reflect Full Stack Developer role in About component

--- app/component/layers/About.jsx
+++ app/component/layers/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div id='about' className='dark:bg-[#0c0c0c] bg-[#fff] side'>
             <div className="content pt-[80px] pb-[95px]">
-                <p className='font-normal sm:text-xl md:text-2xl leading-[130%] dark:text-[#fff] text-[#0c0c0c]'>Hello, I’m <span className='text-[#78cc6d]'>Next Js Developer</span></p>
+                <p className='font-normal sm:text-xl md:text-2xl leading-[130%] dark:text-[#fff] text-[#0c0c0c]'>Hello, I’m <span className='text-[#78cc6d]'>Full Stack Developer</span></p>
                 <h1 className='mt-[18px] max-w-[672px] font-medium text-[35px] sm:text-[45px] md:text-[50px] lg:text-[3.25rem] leading-[130%] dark:text-[#fff] text-[#0c0c0c]'>Web Developer and <span className='text-[#0c0c0c] bg-[#78cc6d] rounded-full px-2 rotate-2 inline-block'>Web Scientist</span>  Based in Dhaka, Bangladesh.</h1>
                 <p className='py-8 font-normal text-sm leading-[170%] dark:text-[#eaeaea] text-[#0c0c0cd3]'>Have worked in a variety of positions, including key developer, solution designer, and data architect.
                     Strong engineering background combined with closely working with business customers.
