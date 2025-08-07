369bbbb6ca84bd643ebe061a28f1042f9f25cfdf
akash100a <arakash968786@gmail.com>
2025-08-07 20:39:39 +0600

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  trailingSlash: true, // optional but recommended for FTP hosting
+
   images: {
     remotePatterns: [
       {
