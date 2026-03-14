eab51ad5420bc5079e85aac23c3c1fd2b03c9dcd
Atiqur Rahman Akash <arakash968786@gmail.com>
2026-03-14 20:16:22 +0600

    fix: correct typo in About component and clean up package-lock.json

--- app/component/layers/About.jsx
+++ app/component/layers/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
             <div className="content pt-[80px] pb-[95px]">
                 <p className='font-normal sm:text-xl md:text-2xl leading-[130%] dark:text-[#fff] text-[#0c0c0c]'>Hello, I’m <span className='text-[#78cc6d]'>Full-Stack Web & Mobile App Developer</span></p>
                 <h1 className='mt-[18px] max-w-[672px] font-medium text-[35px] sm:text-[45px] md:text-[50px] lg:text-[3.25rem] leading-[130%] dark:text-[#fff] text-[#0c0c0c]'>Web Developer and <span className='text-[#0c0c0c] bg-[#78cc6d] rounded-full px-2 rotate-2 inline-block'>Web Scientist</span>  Based in Dhaka, Bangladesh.</h1>
-                <p className='py-8 font-normal text-sm leading-[170%] dark:text-[#eaeaea] text-[#0c0c0cd3]'>Have worked in a variety of positions, including key developer, solution designer, and data architect.
+                <p className='py-8 font-normal text-sm leading-[170%] dark:text-[#eaeaea] text-[#0c0c0cd3]'>Have worked in a variety of positions, including key developer, s\olution designer, and data architect.
                     Strong engineering background combined with closely working with business customers.
                 </p>
                 <div className="flex flex-wrap items-start sm:flex-row justify-start md:items-center gap-2 md:gap-14 lg:gap-2 xl:gap-[70px]">
